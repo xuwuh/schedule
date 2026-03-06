e31fe4d7269f87b140ffe45b59fe9d12ee0d5dde
xuwu <lan.mao.kl@gmail.com>
2026-03-06 16:58:14 +0700

    исправлена ошибка: выводилось ОК при наличии ошибок в расписании

--- validator.py
+++ validator.py
@@ -2,33 +2,34 @@ import json
 from collections  import defaultdict
 
 def validator(schedule_data):
-    errors=[]
+  errors=[]
 
-    teacher_busy=defaultdict(list)
-    auditorium_busy=defaultdict(list)
+  teacher_busy=defaultdict(list)
+  auditorium_busy=defaultdict(list)
 
-    for lesson in schedule_data.get("lessons", []):
-      auditorium=lesson["аудитория"]
-      teacher=lesson["преподаватель"]
-      subject=lesson["предмет"]
-      subject_type=lesson["тип"]
-      day=lesson["день_недели"]
-      time=lesson["время"]
-      id=lesson["uid"]
-      group=lesson["группа"]
-      course=lesson["курс"]
-      key=(day, time)
+  for lesson in schedule_data.get("lessons", []):
+    auditorium=lesson["аудитория"]
+    teacher=lesson["преподаватель"]
+    subject=lesson["предмет"]
+    subject_type=lesson["тип"]
+    day=lesson["день_недели"]
+    time=lesson["время"]
+    id=lesson["uid"]
+    group=lesson["группа"]
+    course=lesson["курс"]
+    key=(day, time)
 
-      for i_teacher, i_id in teacher_busy[key]:
-        if i_teacher==teacher:
-          errors.append(f"{teacher} занят в {day}, {time}, курсом {course} на {subject_type}")
-      teacher_busy[key].append((teacher, id))
+    for i_teacher, i_id in teacher_busy[key]:
+      if i_teacher==teacher:
+        errors.append(f"{teacher} занят в {day}, {time}, курсом {course} на {subject_type}")
+    teacher_busy[key].append((teacher, id))
 
-      for i_auditorium, i_id in auditorium_busy[key]:
-        if i_auditorium==auditorium:
-          errors.append(f"аудитория {auditorium} занята в {day}, {time}")
-      auditorium_busy[key].append((auditorium, id))
-    return errors
+    for i_auditorium, i_id in auditorium_busy[key]:
+      if i_auditorium==auditorium:
+        errors.append(f"аудитория {auditorium} занята в {day}, {time}")
+    auditorium_busy[key].append((auditorium, id))
+
+  return errors
 
 def check_schedule(file_path):
   with open(file_path, "r", encoding='utf-8') as f:
@@ -39,8 +40,8 @@ def check_schedule(file_path):
   if errors:
     for error in errors:
       print(error)
-    else:
-      print("OK")
+  else:
+    print("OK")
 
 if __name__=="__main__":
   schedule_path="lessons.json"
